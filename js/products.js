faa8a7238a4bf0b310bae9bc2e66648b040d456b
luis laffite <luislaffite1@gmail.com>
2021-04-15 09:21:07 +0100

    first commit

--- js/products.js
+++ js/products.js
@@ -4,7 +4,7 @@ const  products = [
             "title": "Apple iPhone 11",
             "image": "./images/products/iphone/iphone3.jpeg",
             "price": 760,
-            "category": "Featured Products"
+            "category": "Featured"
         },
 
         {
@@ -12,28 +12,28 @@ const  products = [
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone2.jpeg",
             "price": 265,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 4,
             "title": "Apple iPhone 11",
             "image": "./images/products/iphone/iphone2.jpeg",
             "price": 850,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 8,
             "title": "Apple iPhone 11",
             "image": "./images/products/iphone/iphone4.jpeg",
             "price": 290,
-            "category": "Featured Products"
+            "category": "Featured"
         },
         {
             "id": 5,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone3.jpeg",
             "price": 250,
-            "category": "Special Products"
+            "category": "Special"
         },
 
         {
@@ -41,7 +41,7 @@ const  products = [
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone4.jpeg",
             "price": 365,
-            "category": "Featured Products"
+            "category": "Featured"
         },
 
 
@@ -50,28 +50,28 @@ const  products = [
             "title": "Apple iPhone 11 Pro",
             "image": "./images/products/iphone/iphone5.jpeg",
             "price": 385,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 11,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone6.jpeg",
             "price": 475,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 13,
             "title": "Apple iPhone 11",
             "image": "./images/products/iphone/iphone6.jpeg",
             "price": 800,
-            "category": "Trending Products"
+            "category": "Trending"
         },
         {
             "id": 12,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone7.jpeg",
             "price": 850,
-            "category": "Special Products"
+            "category": "Special"
         },
 
         {
@@ -79,84 +79,84 @@ const  products = [
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone7.jpeg",
             "price": 360,
-            "category": "Trending Products"
+            "category": "Trending"
         },
         {
             "id": 9,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone5.jpeg",
             "price": 320,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 15,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone8.jpeg",
             "price": 305,
-            "category": "Trending Products"
+            "category": "Trending"
         },
         {
             "id": 16,
             "title": "Samsung Galaxy",
             "image": "./images/products/sumsung/samsung6.jpeg",
             "price": 400,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 17,
             "title": "Samsung Galaxy",
             "image": "./images/products/sumsung/samsung5.jpeg",
             "price": 550,
-            "category": "Trending Products"
+            "category": "Trending"
         },
         {
             "id": 2,
             "title": "Apple iPhone 11",
             "image": "./images/products/iphone/iphone1.jpeg",
             "price": 300,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 18,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone9.jpeg",
             "price": 630,
-            "category": "Trending Products"
+            "category": "Trending"
         },
         {
             "id": 20,
             "title": "Samsung Galaxy",
             "image": "./images/products/sumsung/samsung4.jpeg",
             "price": 270,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 19,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone10.jpeg",
             "price": 250,
-            "category": "Trending Products"
+            "category": "Trending"
         },
         {
             "id": 1,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone1.jpeg",
             "price": 265,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 24,
             "title": "Samsung Galaxy",
             "image": "./images/products/sumsung/samsung2.jpeg",
             "price": 500,
-            "category": "Featured Products"
+            "category": "Featured"
         },
         {
             "id": 21,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone11.jpeg",
             "price": 700,
-            "category": "Trending Products"
+            "category": "Trending"
         },
 
         {
@@ -164,21 +164,21 @@ const  products = [
             "title": "Samsung Galaxy",
             "image": "./images/products/sumsung/samsung1.jpeg",
             "price": 450,
-            "category": "Special Products"
+            "category": "Special"
         },
         {
             "id": 22,
             "title": "Samsung Galaxy",
             "image": "./images/products/sumsung/samsung3.jpeg",
             "price": 460,
-            "category": "Trending Products"
+            "category": "Trending"
         },
         {
             "id": 23,
             "title": "Sony WH-CH510",
             "image": "./images/products/headphone/headphone12.jpeg",
             "price": 600,
-            "category": "Featured Products"
+            "category": "Featured"
         }
 
     ];
@@ -196,17 +196,13 @@ window.addEventListener('DOMContentLoaded' ,function(){
 //filter item
 filterBtns.forEach(function(btn){
     btn.addEventListener("click", function(e) {
-        const category = e.currentTarget.dataset;
+        const category = e.currentTarget.dataset.id;
         const productCategory = products.filter(function (productItem){
-            // console.log(productItem.category)
-
             if(productItem.category === category){
                 return productItem;
-            }     
-            // return productItem      
+            }          
         });
-        // console.log(productCategory);
-        if(category === "all"){
+        if(category === "All"){
             displayProductsItems(products);
         }else{
             displayProductsItems(productCategory)
